078cef9438ee7efe9de5b02cdd0969afea051744
Arthur Inácio <128202143+Inacio04@users.noreply.github.com>
2024-09-06 23:00:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+//console.log("Hello world");  <- só ta aqui ainda pq eu sou fã dele 
+
+function pesquisar(){
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+let campoPesquisa = document.getElementById ("campo-pesquisa").value
+
+console.log(campoPesquisa)
+
+if (campoPesquisa == ""){
+
+    section.innerHTML="<p class='texto-branco'>Digite AMD ou Intel para ver os resultados</p>"
+    return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+let resultados = "";
+let titulo = "";
+let descricao = "";
+let tags = "";
+
+for(let dado of dados){
+
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLocaleLowerCase()
+
+    if (titulo.includes(campoPesquisa) ||  descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+
+        resultados += `
+        <div class="item-resultado">
+                    <h2>${dado.titulo}</h2>
+                        <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}" target="_blank"> Especificações Técnicas </a>
+        </div>
+        ` ; 
+
+    }
+console.log(dado.titulo.includes(campoPesquisa))
+
+} //resumindo  esse mano aqui é um modelo de div só que ele coloca dados no lugar certo
+
+if (!resultados) {
+ resultados = "<p class='texto-branco'>Nada foi encontrado, ou você não digitou nada correspondente.</p>"
+}
+
+
+section.innerHTML = resultados;
+}
+
---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "AMD Ryzen 9 7950X",
+        descricao: "O AMD Ryzen 9 7950X é um processador de 16 núcleos e 32 threads, projetado para oferecer um desempenho excepcional em multitarefas e aplicações profissionais. Ele utiliza a arquitetura Zen 4 e possui um cache L3 de 64 MB.",
+        link: "https://www.amd.com/en/products/cpu/ryzen/7000",
+        tags:""
+      },
+      {
+        titulo: "Intel Core i9-13900K",
+        descricao: "O Intel Core i9-13900K é um processador de alta performance, ideal para gamers e criadores de conteúdo. Com 24 núcleos e 32 threads, ele oferece um desempenho excepcional em tarefas exigentes. Possui clock base de 3.00 GHz GHz e clock turbo de até 5.80 GHz.",
+        link: "https://ark.intel.com/content/www/us/en/ark/products/215559/intel-core-i9-13900k-processor.html",
+        tags:"performance"
+    } ,
+    {
+        titulo : "Intel Core i7-12650H",
+        descricao : "O Intel Core i7-12650H é um processador de 10 núcleos e 16 threads, ideal para notebooks gamers e profissionais que precisam de desempenho em um formato compacto. Ele combina núcleos de alto desempenho e núcleos eficientes para otimizar o consumo de energia.",
+        link: "https://fixed-daphne-b7b.notion.site/Site-pessoal-9e919a2581994dca8b7a7d435618534b",
+        tags:"gamer Desempenho"
+    } ,
+    {
+        titulo : "AMD Ryzen 3 5300G",
+        descricao : "O AMD Ryzen 3 5300G é um processador quad-core com gráficos integrados Radeon Vega 7, ideal para tarefas do dia a dia, como navegar na internet, trabalhar com planilhas e assistir vídeos. Ele oferece um bom desempenho em um preço acessível.",
+        link: "https://fixed-daphne-b7b.notion.site/Site-pessoal-9e919a2581994dca8b7a7d435618534b",
+        tags:"Tarefas"
+    } , 
+    {
+        titulo : "AMD EPYC 9754X",
+        descricao : "O AMD EPYC 9754X é um processador de servidor de 64 núcleos e 128 threads, projetado para oferecer um desempenho excepcional em cargas de trabalho exigentes, como bancos de dados, virtualização e análise de dados.",
+        link: "https://fixed-daphne-b7b.notion.site/Site-pessoal-9e919a2581994dca8b7a7d435618534b",
+        tags:"servidor"
+    } ,// indice 4
+];
